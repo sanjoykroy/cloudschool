5e23eda73e563d2a83b3f4014d45f2ce888d2140
sanjoykroy <sanjoykr78@hotmail.com>
2013-06-20 01:44:33 +0100

    HomeController and HomeControllerTest is updated.

--- cloudschool-webapp/src/main/java/com/realtech/cloudschool/controller/HomeController.java
+++ cloudschool-webapp/src/main/java/com/realtech/cloudschool/controller/HomeController.java
@@ -1,9 +1,17 @@
 package com.realtech.cloudschool.controller;
 
+import com.realtech.cloudschool.model.User;
+import com.realtech.cloudschool.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 
 @Controller
@@ -11,11 +19,17 @@ public class HomeController {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 
+    @Autowired
+    private UserRepository repository;
+
     private static final String HOME_VIEW = "home";
 
     @RequestMapping("/home")
-    public String showHomePage(){
-        LOGGER.info("Showing home page");
+    public String showHomePage(Principal principal, ModelMap map){
+        String userName = principal.getName();
+        LOGGER.info("Showing home page for [{}]", userName);
+        Page<User> users = repository.findByUsername(userName, new PageRequest(0, 1));
+        map.addAttribute("user", users.iterator().next());
         return HOME_VIEW;
     }
 }
--- cloudschool-webapp/src/test/java/com/realtech/cloudschool/controller/HomeControllerTest.java
+++ cloudschool-webapp/src/test/java/com/realtech/cloudschool/controller/HomeControllerTest.java
@@ -1,13 +1,29 @@
 package com.realtech.cloudschool.controller;
 
+import com.realtech.cloudschool.model.User;
+import com.realtech.cloudschool.repository.UserRepository;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.web.server.MockMvc;
 import org.springframework.web.servlet.View;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.security.Principal;
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.any;
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.server.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.server.setup.MockMvcBuilders.standaloneSetup;
 import static org.springframework.test.web.server.result.MockMvcResultMatchers.view;
@@ -16,8 +32,10 @@ import static org.springframework.test.web.server.request.MockMvcRequestBuilders
 public class HomeControllerTest {
 
     private static final String EXPECTED_HOME_VIEW = "home";
-
+    @InjectMocks
     private HomeController controller;
+    @Mock
+    private UserRepository repository;
     @Mock
     private View mockView;
     private MockMvc mockMvc;
@@ -31,8 +49,28 @@ public class HomeControllerTest {
 
     @Test
     public void shouldShowHomePage() throws Exception {
-        this.mockMvc.perform(get("/home"))
+        User user =  getFakeUser();
+        Page<User> users = new PageImpl<User>(Arrays.asList(user));
+        Pageable pageable = new PageRequest(0, 1);
+        when(repository.findByUsername("testUser", pageable)).thenReturn(users);
+        Principal principal = new Principal() {
+            @Override
+            public String getName() {
+                return "testUser";
+            }
+        };
+
+        this.mockMvc.perform(get("/home").principal(principal))
+                .andExpect(model().attribute("user", user))
                 .andExpect(view().name(containsString(EXPECTED_HOME_VIEW)))
                 .andExpect(status().isOk());
+
+        verify(repository).findByUsername("testUser", pageable);
+    }
+
+    private User getFakeUser(){
+        User user = new User();
+        user.setUsername("testUser");
+        return user;
     }
 }
